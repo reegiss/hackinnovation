e45dadbdf0f9872fe475ddda3c3216c65ce234e2
Regis Araujo Melo <regis@r3tecnologia.net>
2019-03-31 00:32:12 -0300

    Tela esqueceu a senha

---
+++ app/src/main/java/br/hackinnovation/appintegramobi/activity/ResetPasswordActivity.java
@@ -0,0 +1,23 @@
+package br.hackinnovation.appintegramobi.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import br.hackinnovation.appintegramobi.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
+        setTitle("Recuperar sua senha");
+    }
+}
