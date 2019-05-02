690fb51d2371067136fa935beaadd202e0fb0ed4
Regis Araujo Melo <regis@r3tecnologia.net>
2019-05-02 00:26:23 -0300

    ultimo commit

---
+++ app/src/main/java/br/hackinnovation/appintegramobi/activity/QRCodeGenerate.java
@@ -0,0 +1,47 @@
+package br.hackinnovation.appintegramobi.activity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import br.hackinnovation.appintegramobi.R;
+
+public class QRCodeGenerate extends AppCompatActivity {
+
+    private ImageView qrCode;
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_generate);
+
+
+        qrCode = findViewById(R.id.imageView);
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth != null) {
+            String text = mAuth.getCurrentUser().getUid();
+            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            try {
+                BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                qrCode.setImageBitmap(bitmap);
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
